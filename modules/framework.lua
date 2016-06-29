3d92954737b74a9e35643eb8a227650691fd2c7b
root <root@clm-pun-021273.bmc.com>
2016-06-29 03:10:51 -0400

    copy rights added

--- modules/framework.lua
+++ modules/framework.lua
@@ -1,4 +1,25 @@
--- [author] Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
+-- Copyright 2015 Boundary, Inc.
+-- --
+-- -- Licensed under the Apache License, Version 2.0 (the "License");
+-- -- you may not use this file except in compliance with the License.
+-- -- You may obtain a copy of the License at
+-- --
+-- --    http://www.apache.org/licenses/LICENSE-2.0
+-- --
+-- -- Unless required by applicable law or agreed to in writing, software
+-- -- distributed under the License is distributed on an "AS IS" BASIS,
+-- -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- -- See the License for the specific language governing permissions and
+-- -- limitations under the License.-
+--
+-- ---------------
+-- -- A Boundary Plugin Framework for easy development of custom Boundary.com plugins.
+-- --
+-- -- [https://github.com/boundary/boundary-plugin-framework-lua](https://github.com/boundary/boundary-plugin-framework-lua)
+-- -- @module Boundary Plugin Framework for LUA
+-- -- @author Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
+-- -- @license Apache 2.0
+-- -- @copyright 2015 Boundary, Inc
 local boundary = require('boundary')
 local Emitter = require('core').Emitter
 local Error = require('core').Error
